b0de5294bf291b9381a7af88ed604d1b5924ef1e
marcosoliveira <marcos.oliveira@wevo.com.br>
2022-10-16 13:01:54 -0300

    Enhancements controller health check

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -5,16 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * @author marcos
  */
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
-
-    @GetMapping()
-    public ResponseEntity<Object> status() {
-        return ResponseEntity.ok().build();
+    @GetMapping("version")
+    public ResponseEntity<String> status() throws IOException {
+        Properties props = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yaml");
+        props.load(inputStream);
+        return ResponseEntity.ok(
+                props.getProperty("build.name")
+                + " - "
+                + props.getProperty("build.version")
+        );
     }
-
 }
