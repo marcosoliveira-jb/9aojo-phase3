c521a443254e809f7b6fb9a7b693082fafa1a25a
Thyago Henrique Pacher <thyago.pacher@gmail.com>
2022-10-20 19:53:29 -0300

    telas ordem serviço

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -8,8 +8,17 @@ class UserForm extends StatelessWidget{
   final _form = GlobalKey<FormState>();
   final Map<String, dynamic> _formData = {};
 
+  void _loadFormData(User user){
+
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final User user = ModalRoute.of<User>(context)?.settings.arguments;
+    
+    _loadFormData(user);
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de Prestador de Serviço'),
